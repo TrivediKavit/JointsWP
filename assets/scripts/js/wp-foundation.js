b8ff934fa923412e7c5cc80f51835d8a40b7ace8
Kavit Trivedi <knt1993@gmail.com>
2019-01-03 14:36:50 +0530

    - Update to Foundation 6.5.1

--- assets/scripts/js/wp-foundation.js
+++ assets/scripts/js/wp-foundation.js
@@ -10,7 +10,7 @@ var DATEPICKER_FIELD = ".codesm-datepicker-field";
 jQuery(document).ready(function() {
 	
     // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
+	jQuery('.accordion p:empty, .orbit p:empty').remove();
 
 	// Adds Flex Video to YouTube and Vimeo Embeds
 	jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
--- parts/nav-topbar.php
+++ parts/nav-topbar.php
@@ -7,12 +7,28 @@
 ?>
 
 <div class="top-bar" id="main-menu">
-	<div class="top-bar-left">
-		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
-	</div>
-	<div class="top-bar-right">
-		<?php joints_top_nav(); ?>
+	<div class="row">
+		<div class="columns large-12">
+			<div class="top-bar-left">
+				<ul class="menu">
+					<li class="menu-text">
+						<a href="<?php echo home_url(); ?>">
+							<?php $logo = "/assets/images/logo.png"; ?>
+							<?php if(file_exists(get_template_directory().$logo)): ?>
+								<img src="<?php echo get_template_directory_uri().$logo; ?>" alt="<?php bloginfo('name'); ?>">
+							<?php else: ?>
+								<h2 class="site-title"><?php bloginfo('name'); ?></h2>
+								<p class="site-description"><?php bloginfo('description'); ?></p>
+							<?php endif; ?>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="top-bar-right text-center large-text-right">
+				<div class="primary-menu">
+					<?php joints_top_nav(); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
